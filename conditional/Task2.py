d9436d9a5fc166472fa5003727ede1b56fab2a82
JibinAji21 <jibinaji26@gmail.com>
2024-08-09 15:58:23 +0530

    if else if & nested if

---
+++ conditional/Task2.py
@@ -0,0 +1,29 @@
+#if else if 
+
+#a=input(("enter first number"))
+#b=input(("enter second number"))
+#if a>b:
+ #   print('a is greater')
+#elif a==b:
+ #   print('a equal to b')
+#else:
+ #   print('b is greater')
+
+#nested if
+
+a=input(("enter first number"))
+b=input(("enter second number"))
+c=input(("enter third number"))
+if a>b:
+    if a>c:
+        print('a is large')
+    else:
+        print('c is large')
+else:
+    if b>c:
+        print('b is large')
+    else:
+        print('c is large')
+
+
+
