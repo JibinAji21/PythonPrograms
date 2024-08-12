e8dc44ff97780b443db5501b34e7b339ba1dd26f
JibinAji21 <jibinaji26@gmail.com>
2024-08-12 16:04:16 +0530

    python task

---
+++ task3.py
@@ -0,0 +1,74 @@
+#question1
+
+# salary=int(input("enter your salary"))
+# year=int(input("enter year of experience"))
+# if (year>=5):
+#     print("your net bonus amount is",salary*0.05)
+#     print("total amount", (salary*0.05)+salary)
+# else:
+#     print("no bonus")
+
+ #question2
+
+# unit=int (input("enter unit used"))
+# if(unit<=100):
+#     print("no charge")
+# elif(100<unit<=200): 
+#     print("charge",(unit-100)*5)
+# else:
+#     print("charge",((unit-200)*10)+500) 
+
+#question3      
+
+# number=int(input("enter a number"))
+# if(number==1):
+#     print("sunday")
+# elif(number==2):
+#     print("monday")
+# elif(number==3):
+#     print("tuesday")
+# elif(number==4):
+#     print("wednesday")
+# elif(number==5):
+#     print("thursday") 
+# elif(number==6):
+#     print("friday")
+# elif(number==7):
+#     print("saturday")  
+# else:
+#     print("invalid") 
+     
+#question4
+
+
+# city=input("enter your city")
+# city=city.lower()
+# if(city=="delhi"):
+#     print("Redfort")
+# elif(city=="agra"):
+#     print("Taj mahal")
+# elif(city=="jaipur"):
+#     print("Jai mahal") 
+# else:
+#     print("invalid input")  
+city=input("enter your city")
+# city=city.lower()
+# if(city=="delhi"):
+#     print("Redfort")
+# elif(city=="agra"):
+#     print("Taj mahal")
+# elif(city=="jaipur"):
+#     print("Jai mahal") 
+# else:
+#     print("invalid input")  
+
+
+#question5
+
+number=int(input("enter a number"))
+
+
+
+
+
+    
