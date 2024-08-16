b3e6080da3b21dfcb9127ad6b9b4050ffeeb0db1
JibinAji21 <jibinaji26@gmail.com>
2024-08-16 16:23:59 +0530

    nested for loop

---
+++ nestedfor.py
@@ -0,0 +1,20 @@
+# for i in range(5):
+#     print("i=",i)
+#     for j in range (7):
+#         print(j,end="")
+#     print()
+
+# output: 
+       
+# i= 0
+# 0123456
+# i= 1
+# 0123456
+# i= 2
+# 0123456
+# i= 3
+# 0123456
+# i= 4
+# 0123456
+
+
