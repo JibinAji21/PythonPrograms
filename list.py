51907d8797fa73bfced15f95a84d6e43b8c7d9a5
JibinAji21 <jibinaji26@gmail.com>
2024-08-19 16:02:52 +0530

    list and list methods

---
+++ list.py
@@ -0,0 +1,57 @@
+# empty list:
+
+# l=[]
+# print(len(l))
+
+# output = 0
+
+# length of a list:
+
+# l=[1,2,'abc',10.5,1]
+# print(len(l))
+
+# output =5
+
+
+# l=[1,2,3,4]
+# if 1 in l:
+#     print('yes')
+# else:
+#     print('not')
+
+# output = yes
+
+
+# l=[1,2,3,'abc',2.5,1]
+# for i in l:
+#     print(i)
+
+# output
+
+# 1
+# 2
+# 3
+# abc
+# 2.5
+# 1
+
+# list=[1,2,3,'xyz',6.5]
+# a=input("enter a value")
+# if a in l:
+#     print("yes")
+
+
+# update a value present in list
+
+# list=[1,2,'xyz',3.5,4,5]
+# print(list[0])
+# print(list[5])
+# list[2]=15
+# print(list)
+
+# # output
+
+# 1
+# 5
+# [1, 2, 15, 3.5, 4, 5]
+
---
+++ listmethods.py
@@ -0,0 +1,148 @@
+# value adding methods
+
+# 1) append:
+
+# l=[]
+# l.append(5)
+# l.append(10)
+# l.append(20)
+# l.append('abc')
+# l.append([10,11,12])
+# print(l)
+
+# output
+
+# [5, 10, 20, 'abc', [10, 11, 12]]
+
+# 2) extend:
+
+
+# l=[]
+# l.append(5)
+# l.append(10)
+# l.append(20)
+# l.append('abc')
+# l.append([10,11,12])
+# l.extend([3,4,5])
+# print(l)
+
+# output
+
+# [5, 10, 20, 'abc', [10, 11, 12], 3, 4, 5]
+
+
+# 3) insert:
+
+# l=[]
+# l.append(5)
+# l.append(10)
+# l.append(20)
+# l.append('abc')
+# l.append([10,11,12])
+# l.extend([3,4,5])
+# l.insert(2,"hy")
+# print(l)
+
+# outout
+
+# [5, 10, 'hy', 20, 'abc', [10, 11, 12], 3, 4, 5]
+
+
+# value removing methods
+
+# 1) clear
+
+# l=[10,20,30,'abc']
+# l.clear()
+# print(l)
+
+# output
+
+# []
+# empty list all values will be cleared
+
+# 2) pop
+
+# l=[10,20,30,'abc']
+# l.pop()
+# print(l)
+
+# output
+
+# [10, 20, 30]
+
+# example 2)
+
+# l=[10,20,30,'abc']
+# l.pop(2)
+# print(l)
+
+# output
+
+# [10, 20, 'abc']
+
+
+# 3) remove
+
+# l=[10,20,30,'abc']
+# l.remove(10)
+# print(l)
+
+# output
+
+# [20, 30, 'abc']
+
+
+
+# sort
+
+
+# l=[10,355,5]
+# l.sort()
+# print(l)
+
+# output
+
+# [5, 10, 355]
+
+# reverse
+
+# l=[10,15,500,300]
+# l.reverse()
+# print(l)
+
+# output
+
+# [300, 500, 15, 10]
+
+
+# index
+
+# l=[10,15,'a',20]
+# print(l.index('a'))
+
+# output
+
+# 2
+
+
+# count
+
+# l=[10,11,12,13,14]
+# print(l.count(14))
+
+# output
+
+# 1
+
+
+
+
+
+
+
+
+
+
+
+
