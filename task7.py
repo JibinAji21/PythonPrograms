914664d7bc29503631cf2afa86e3170e75143f45
JibinAji21 <jibinaji26@gmail.com>
2024-08-21 15:50:39 +0530

    list task

---
+++ task7.py
@@ -0,0 +1,43 @@
+# question no 1
+
+# sum of odd numbers and even numbers
+
+# l=[1,2,3,4,5,6]
+# a=l[0]
+# s=0
+# b=0
+# for i in l:
+#     if i%2==1:
+#         s+=i
+#     elif i%2==0:
+#         b+=i
+# print("sum of odd numbers",s)
+# print("sum of even numbers=",b)
+
+# output
+
+# sum of odd numbers 9
+# sum of even numbers= 12
+
+
+# question no 2
+
+# sum of numbers in string
+
+# l=[10,11,5.8,'abc']
+# s=0
+# for i in l:
+#     s+=1
+#     print(s)
+
+# question no 3
+
+# divisible by 2 and square of that numbers
+
+l=[1,2,5,8,9,11,16,12]
+a=[0]
+b=[0]
+for i in l:
+    if i%2==0:
+        print(i)
+    
