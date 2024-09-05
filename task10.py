25711375fead7966e27ffe6d0eb0524ca8f30310
JibinAji21 <jibinaji26@gmail.com>
2024-09-05 13:38:37 +0530

    dict task

---
+++ task10.py
@@ -0,0 +1,38 @@
+# num=int(input("enter a number"))
+# while num>0:
+#     d=num%10
+#     print(d)
+#     num//=10
+
+    # output
+
+# enter a number123
+# 3
+# 2
+# 1
+
+
+# num=int(input("enter a number"))
+# numbers={0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine'}
+# s=''
+# while num>0:
+#     d=num%10
+#     s=numbers[d]+' '+s
+#     num//=10
+# print(s)
+
+# output
+
+# enter a number12345
+# one two three four five 
+
+
+
+# l=[{'name':'a','age':20,'lang':['mal','eng']}]
+# user=input("enter lang")
+# l[0]['lang'].append(user)
+# l[0]['lang'].pop(0)
+# print(l)
+
+# enter lang hindi
+# [{'name': 'a', 'age': 20, 'lang': ['eng', 'hindi']}]
