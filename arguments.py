75b32a20b57aade9c2565b78a91a7304220e2866
JibinAji21 <baluns2003@gmail.com>
2024-09-10 13:30:22 +0530

    function arguments tasks

---
+++ arguments.py
@@ -0,0 +1,46 @@
+# Types of arguments
+
+# 1)Positional Argument
+# 2)Function With Keyword Argument
+# 3)Default Argument
+# 4)Arbitray Argument
+# 5)Arbitary Keyword Argument
+
+
+# 1) positional argument example
+# ...........................................
+
+# def dtls(name,age):
+#     print("name=",name)
+#     print("age=",age)
+
+# dtls("anu",20)
+# dtls("manu",22)
+# dtls(22,"abhi")
+
+# output
+
+# name= anu
+# age= 20
+# name= manu
+# age= 22
+# name= 22
+# age= abhi
+# ................................................
+
+# 2) Function with keyword argument example
+
+
+# def dtls(name,age):
+#     print("name=",name)
+#     print("age=",age)
+
+# dtls(age=22,name="jomon")
+
+# # output
+
+# # name= jomon
+# # age= 22
+# .....................................................
+
+
