1ceed968e58679f97a7ac97fe4fef5b0da32697d
JibinAji21 <baluns2003@gmail.com>
2024-09-25 13:37:44 +0530

    file handling tasks

--- filehandling/filehandling.py
+++ filehandling/filehandling.py
@@ -1,4 +1,4 @@
-f=open('PythonPrograms/filehandling/demo.txt','a')
+# f=open('PythonPrograms/filehandling/demo.txt','a')
 # f.write('welcome to all')
 # f.write('hope you all are fine')
 # print(f.read())
@@ -16,6 +16,8 @@ f=open('PythonPrograms/filehandling/demo.txt','a')
 # # print(b)
 # print(c)
 
+#reverse of a string 
+
 # d=f.readlines()
 # f.seek(0)
 # for i in range(len(d)):
@@ -23,5 +25,73 @@ f=open('PythonPrograms/filehandling/demo.txt','a')
 #     # print(a)
 #     print(a[::-1])
 
-f.write('bad time\n')
-f.write('aaaaaaaaaaaaaaa')
+# f.write('bad time\n')
+# f.write('aaaaaaaaaaaaaaa')
+
+
+# f=open('PythonPrograms/filehandling/demo.txt','r')
+# d=f.readlines()
+# f.seek(0)
+# letter=0
+# for i in range(len(d)):
+#     a=f.readline().strip()
+#     for i in a:
+#         if i !=' ':
+#             letter+=1
+# print(letter)
+
+# output :13
+
+
+
+
+# f=open('PythonPrograms/filehandling/demo.txt','r')
+# l=f.readlines()
+# f.seek(0)
+# letter=0
+# cap=0
+# for i in range(len(l)):
+#     a=f.readline().strip()
+#     for i in a:
+#         if i !=' ':
+#             if i.isupper():
+#                 cap+=1
+#             letter+=1
+# print('total no of letter',letter)
+# print('total capital letters',cap)
+# print('total small letters',letter-cap)
+
+# # output
+
+# total no of letter 13
+# total capital letters 1
+# total small letters 12
+
+
+
+# f=open('PythonPrograms/filehandling/demo.txt','r')
+# l=f.readlines()
+# f.seek(0)
+# letter=0
+# cap=0
+# words=0
+# for i in range(len(l)):
+#     a=f.readline().strip()
+#     s=a.split(' ')
+#     for i in s:
+#         if i!='':
+#             words+=1
+#     for i in a:
+#         if i !=' ':
+#             if i.isupper():
+#                 cap+=1
+#                 letter+=1
+# print('total no of letter',letter)
+# print('total capital letters',cap)
+# print('total small letters',letter-cap)
+# print(words)
+
+  
+
+    
+        
--- filehandling/multiplicationtable.py
+++ filehandling/multiplicationtable.py
@@ -1,5 +1,15 @@
-f=open('PythonPrograms/filehandling/mult.txt','w')
-num=int(input('enter a number'))
+# f=open('PythonPrograms/filehandling/mult.txt','w')
+# num=int(input('enter a number'))
+# for i in range(1,11):
+#     # print(i,'*',num,'=',i*num)
+#     f.write(str(i)+'*'+str(num)+'='+str(i*num)+"\n")
+
+
+#    Task
+
+f=open('PythonPrograms/filehandling/mult2.txt','w')
+number=int(input("Enter a number : "))
 for i in range(1,11):
-    # print(i,'*',num,'=',i*num)
-    f.write(str(i)+'*'+str(num)+'='+str(i*num)+"\n")
+    for j in range(1,number+1):
+        f.write(str(i)+'*'+str(j)+'='+str(j*i)+'\t')
+    f.write('\n')
