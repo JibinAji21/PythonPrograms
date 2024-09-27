9aabb5584dff29d6a4c41b9b3c1b042b8f4a9e9f
JibinAji21 <jibinaji26@gmail.com>
2024-09-27 13:33:27 +0530

    object oriented programming

---
+++ objectorientedprogramming/concepts in oops.py
@@ -0,0 +1,25 @@
+# 1) class - blue print used to create a object
+
+# syntax - class class_name:
+#                 statement
+
+# 2) object - instance of class is called object
+
+# syntax - var_name = class_name
+
+# 3) method - functions created in a class is called method
+
+# syntax - def method name():
+#                 statement
+
+
+# example:
+
+class synnefo:
+    def python():
+        print('python in synnefo')
+    def php ():
+        print('php in synnefo')
+
+std1=synnefo
+std1.python()
---
+++ objectorientedprogramming/example1bank.py
@@ -0,0 +1,62 @@
+# class bank:
+#     def __init__(self):
+#         self.bal=0
+#     def deposit(self,amt):
+#         self.bal+=amt
+#         print('amount added')
+#     def withdraw(self,amt):
+#         self.bal-=amt
+#         print('amount withdraw')
+#     def display(self):
+#         print('remaining balace',self.bal)
+# user1=bank()
+# user1.deposit(25000)
+# user1.withdraw(1000)
+# user1.display()
+
+# # output user 1
+
+# # amount added
+# # amount withdraw
+# # remaining balace 24000
+
+# user2=bank()
+# user2.deposit(10000)
+# user2.withdraw(5000)
+# user2.display()
+
+# output user 2
+
+# amount added
+# amount withdraw
+# remaining balace 5000
+# .............................................................................
+
+# sample program using types of arguments
+
+
+# 1) using positional argument
+
+# class demo:
+#     def __init__(self,a):
+#         print(a)
+# obj=demo(10000)
+
+# 2) using keyword argument
+
+# class demo:
+#     def __init__(self,a,b):
+#         print(a)
+#         print(b)
+# obj=demo(a=20,b=35)
+
+# 3 ) using arbitary argument
+
+# class demo:
+#     def __init__(self,*a):
+#         print(a)
+# obj=demo(20,13)
+
+
+
+
