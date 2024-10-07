4dd895fe25bff9e3dbbdab366a95df936ae9c104
JibinAji21 <jibinaji26@gmail.com>
2024-10-07 13:51:30 +0530

    database project

--- database/table.py
+++ database/table.py
@@ -10,5 +10,155 @@ try:
 except:
     print('table exist')
 
-con.execute("insert into std(roll_no,name,age,mark)values(1,'manu',20,100),(2,'jibin',21,95)")
-con.commit()
+# con.execute("insert into std(roll_no,name,age,mark)values(1,'manu',20,100),(2,'jibin',21,95)")
+# # con.commit()
+
+# roll=int(input("enter roll_no"))
+# name=input("enter name")
+# age=int(input("enter age"))
+# mark=float(input("enter mark"))
+# con.execute('insert into std(roll_no,name,age,mark)values(?,?,?,?)',(roll,name,age,mark))
+# con.commit()
+# .......................................................................................
+
+
+# limit=int(input('enter students limit'))
+# for i in range(limit):
+#     roll=int(input("enter roll_no"))
+#     name=input("enter name")
+#     age=int(input("enter age"))
+#     mark=float(input("enter mark"))
+#     con.execute('insert into std(roll_no,name,age,mark)values(?,?,?,?)',(roll,name,age,mark))
+#     con.commit()
+# .................................................................
+
+
+# to display items in a table:
+
+# data=con.execute("select * from std")
+# for i in data:
+#     print(i)
+
+    # output
+
+
+# (1, 'manu', 20, 100.0)
+# (1, 'manu', 20, 100.0)
+# (2, 'jibin', 21, 95.0)
+# (7, 'samuel', 19, 86.0)
+# (10, 'sunil', 22, 99.0)
+# (11, 'krishnan', 21, 100.0)
+# .................................................................................
+
+
+# to display a specific field in a table:(name,age,mark,roll_no)
+
+
+# data=con.execute("select name from std")
+# for i in data:
+#     print(i)
+
+    # output
+
+    # ('manu',)
+    # ('manu',)
+    # ('jibin',)
+    # ('samuel',)
+    # ('sunil',)
+    # ('krishnan',
+# ...................................................................................
+
+
+
+# data=con.execute("select * from std")
+# for i in data:
+#     print('{:<15}{:<15}{:<15}{:<15}'.format('roll_no','name','age','mark'))
+#     print('-'*70)
+#     for i in data:
+#         print('{:<15}{:<15}{:<15}{:<15}'.format(i[0],i[1],i[2],i[3]))
+
+# output
+
+# roll_no        name           age            mark           
+# ----------------------------------------------------------------------
+# 1              manu           20             100.0          
+# 2              jibin          21             95.0           
+# 7              samuel         19             86.0           
+# 10             sunil          22             99.0           
+# 11             krishnan       21             100.0      
+
+# ......................................................................................................
+
+# example 1:
+
+# data=con.execute("select * from std where name='samuel'")
+# for i in data:
+#     print(i)
+
+# # output
+
+# (7, 'samuel', 19, 86.0)
+  
+# example 2:
+
+# data=con.execute("select * from std where roll_no='1'")
+# for i in data:
+#     print(i)
+
+#     # output
+
+#     (1, 'manu', 20, 100.0)
+# ..........................................................................
+
+# method -1
+
+
+# user=int(input("enter student roll_no"))
+# data=con.execute("select * from std where roll_no='1'")
+# for i in data:
+#     if i[0]==user:
+#         print(i)
+
+# method - 2
+
+
+# user=int(input("enter student roll_no"))
+# data=con.execute("select * from std where roll_no=?",(user,))
+# for i in data:
+#     print(i)
+
+# # output
+
+# enter student roll_no1
+# (1, 'manu', 20, 100.0)
+# (1, 'manu', 20, 100.0)
+# .........................................................................
+
+# to update a value in database:
+
+# con.execute("update std set name ='jinsha' where name='manu'")
+# con.commit()
+
+
+# name=input("enter name")
+# new_name=input("enter new name")
+# con.execute("update std set name =? where name=?",(new_name,name))
+# con.commit()
+# .................................................................................
+
+
+# to delete a value from database:
+
+# con.execute("delete from std where roll_no=1")
+# con.commit() 
+
+
+# user=int(input("enter roll_no to delete"))
+# con.execute("delete from std where roll_no=?",(user,))
+# con.commit() 
+
+
+
+
+
+
---
+++ project_database.py
@@ -0,0 +1,87 @@
+import sqlite3
+con=sqlite3.connect('employee.db')
+try:
+    con.execute("create table emp(id int,name text,age int,place text,post text,phone int)")
+except:
+    print("table already exist")
+
+    while True:
+        print(
+            '''
+            1.Add Employee
+            2.View Employee
+            3.Update Employee
+            4.Delete Employee
+            5.Search Employee
+            6.Exit
+        '''
+        )
+        choice=int(input("enter your choice"))
+        if choice==1:
+            id=int(input("enter employee id"))
+            name=input("enter employee name")
+            age=int(input("enter employee age"))
+            place=(input("enter employee place"))
+            post=input("enter employee post")
+            phone=int(input("enter employee phone no"))
+            con.execute('insert into emp(id,name,age,place,post,phone)values(?,?,?,?,?,?)',(id,name,age,place,post,phone))
+            con.commit()
+        elif choice==2:
+            data=con.execute("select * from emp")
+            for i in data:
+                print('{:<15}{:<15}{:<15}{:<15}{:15}{:15}'.format('id','name','age','place','post','phone'))
+                print('-'*70)
+                for i in data:
+                    print('{:<15}{:<15}{:<15}{:<15}{:<15}{:<15}'.format(i[0],i[1],i[2],i[3],i[4],i[5]))
+        elif choice==3:
+            while True:
+                print(
+                    '''
+                    1.Update Employee Name
+                    2.Update Employee Age
+                    3.Update Employee place
+                    4.Update Employee Post
+                    5.Update Employee Phone no
+                    6.Exit
+                '''
+                )
+                sub_choice=int(input("enter your choice"))
+                if sub_choice==1:
+                    name=input("enter current name")
+                    new_name=input("enter new name")
+                    con.execute("update std set name =? where name=?",(new_name,name))
+                    con.commit()
+                elif sub_choice==2:
+                    age=input("enter current age")
+                    new_age=input("enter new age")
+                    con.execute("update std set age =? where age=?",(new_age,age))
+                    con.commit()
+                elif sub_choice==3:
+                    place=input("enter current place")
+                    new_place=input("enter new place")
+                    con.execute("update std set place =? where place=?",(new_place,place))
+                    con.commit()
+                elif sub_choice==4:
+                    post=input("enter current post")
+                    new_post=input("enter new post")
+                    con.execute("update std set post =? where post=?",(new_post,post))
+                    con.commit()
+                elif sub_choice==5:
+                    phone=input("enter current phone")
+                    new_phone=input("enter new phone")
+                    con.execute("update std set post =? where post=?",(new_phone,phone))
+                    con.commit()
+                elif sub_choice==6:
+                    break
+
+
+
+                    
+
+                
+
+                
+
+
+
+
